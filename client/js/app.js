15b5469ab71f588c4e2ae3bae551e1c875765be8
leons <Leonstolz72@gmail.com>
2026-04-11 23:49:28 +0200

    setup, style: erste html für den VB-Workflow erstellt

---
+++ client/js/app.js
@@ -0,0 +1,8 @@
+
+vb_workflow_erstellen = () => {
+  setTimeout(() => window.location.href = "../html/create_workflow_vb.html",500)
+};
+
+nav_hauptmenu = () => {
+  window.location.href = "../html/index.html"
+};
